8697d451b4d1bb25bb0d772e857f8a3802cdc279
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@intelligencia.ai>
2021-05-22 18:43:17 +0300

    Update basic-setup to pluto settings

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,62 +2,25 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var view = require('@codemirror/view');
 var state = require('@codemirror/state');
-var history = require('@codemirror/history');
-var fold = require('@codemirror/fold');
-var language = require('@codemirror/language');
+var streamParser = require('@codemirror/stream-parser');
+var julia = require('@codemirror/legacy-modes/mode/julia');
 var gutter = require('@codemirror/gutter');
+var view = require('@codemirror/view');
+var history = require('@codemirror/history');
 var commands = require('@codemirror/commands');
+var highlight = require('@codemirror/highlight');
+var language = require('@codemirror/language');
+var rectangularSelection = require('@codemirror/rectangular-selection');
+var fold = require('@codemirror/fold');
 var matchbrackets = require('@codemirror/matchbrackets');
 var closebrackets = require('@codemirror/closebrackets');
-var search = require('@codemirror/search');
 var autocomplete = require('@codemirror/autocomplete');
+var search = require('@codemirror/search');
 var comment = require('@codemirror/comment');
-var rectangularSelection = require('@codemirror/rectangular-selection');
-var highlight = require('@codemirror/highlight');
-var lint = require('@codemirror/lint');
 
-/**
-This is an extension value that just pulls together a whole lot of
-extensions that you might want in a basic editor. It is meant as a
-convenient helper to quickly set up CodeMirror without installing
-and importing a lot of packages.
-
-Specifically, it includes...
-
- - [the default command bindings](https://codemirror.net/6/docs/ref/#commands.defaultKeymap)
- - [line numbers](https://codemirror.net/6/docs/ref/#gutter.lineNumbers)
- - [special character highlighting](https://codemirror.net/6/docs/ref/#view.highlightSpecialChars)
- - [the undo history](https://codemirror.net/6/docs/ref/#history.history)
- - [a fold gutter](https://codemirror.net/6/docs/ref/#fold.foldGutter)
- - [custom selection drawing](https://codemirror.net/6/docs/ref/#view.drawSelection)
- - [multiple selections](https://codemirror.net/6/docs/ref/#state.EditorState^allowMultipleSelections)
- - [reindentation on input](https://codemirror.net/6/docs/ref/#language.indentOnInput)
- - [the default highlight style](https://codemirror.net/6/docs/ref/#highlight.defaultHighlightStyle) (as fallback)
- - [bracket matching](https://codemirror.net/6/docs/ref/#matchbrackets.bracketMatching)
- - [bracket closing](https://codemirror.net/6/docs/ref/#closebrackets.closeBrackets)
- - [autocompletion](https://codemirror.net/6/docs/ref/#autocomplete.autocompletion)
- - [rectangular selection](https://codemirror.net/6/docs/ref/#rectangular-selection.rectangularSelection)
- - [active line highlighting](https://codemirror.net/6/docs/ref/#view.highlightActiveLine)
- - [active line gutter highlighting](https://codemirror.net/6/docs/ref/#gutter.highlightActiveLineGutter)
- - [selection match highlighting](https://codemirror.net/6/docs/ref/#search.highlightSelectionMatches)
- - [search](https://codemirror.net/6/docs/ref/#search.searchKeymap)
- - [commenting](https://codemirror.net/6/docs/ref/#comment.commentKeymap)
- - [linting](https://codemirror.net/6/docs/ref/#lint.lintKeymap)
-
-(You'll probably want to add some language package to your setup
-too.)
-
-This package does not allow customization. The idea is that, once
-you decide you want to configure your editor more precisely, you
-take this package's source (which is just a bunch of imports and
-an array literal), copy it into your own code, and adjust it as
-desired.
-*/
 const basicSetup = [
     gutter.lineNumbers(),
-    gutter.highlightActiveLineGutter(),
     view.highlightSpecialChars(),
     history.history(),
     fold.foldGutter(),
@@ -79,20 +42,80 @@ const basicSetup = [
         ...fold.foldKeymap,
         ...comment.commentKeymap,
         ...autocomplete.completionKeymap,
-        ...lint.lintKeymap
-    ])
+        //    ...lint.lintKeymap,
+    ]),
 ];
 
-Object.defineProperty(exports, 'EditorView', {
+Object.defineProperty(exports, 'Compartment', {
     enumerable: true,
     get: function () {
-        return view.EditorView;
+        return state.Compartment;
+    }
+});
+Object.defineProperty(exports, 'EditorSelection', {
+    enumerable: true,
+    get: function () {
+        return state.EditorSelection;
     }
 });
 Object.defineProperty(exports, 'EditorState', {
     enumerable: true,
     get: function () {
         return state.EditorState;
+    }
+});
+Object.defineProperty(exports, 'StreamLanguage', {
+    enumerable: true,
+    get: function () {
+        return streamParser.StreamLanguage;
+    }
+});
+Object.defineProperty(exports, 'julia', {
+    enumerable: true,
+    get: function () {
+        return julia.julia;
+    }
+});
+Object.defineProperty(exports, 'EditorView', {
+    enumerable: true,
+    get: function () {
+        return view.EditorView;
+    }
+});
+Object.defineProperty(exports, 'keymap', {
+    enumerable: true,
+    get: function () {
+        return view.keymap;
+    }
+});
+Object.defineProperty(exports, 'history', {
+    enumerable: true,
+    get: function () {
+        return history.history;
+    }
+});
+Object.defineProperty(exports, 'historyKeymap', {
+    enumerable: true,
+    get: function () {
+        return history.historyKeymap;
+    }
+});
+Object.defineProperty(exports, 'defaultKeymap', {
+    enumerable: true,
+    get: function () {
+        return commands.defaultKeymap;
+    }
+});
+Object.defineProperty(exports, 'indentLess', {
+    enumerable: true,
+    get: function () {
+        return commands.indentLess;
+    }
+});
+Object.defineProperty(exports, 'indentMore', {
+    enumerable: true,
+    get: function () {
+        return commands.indentMore;
     }
 });
 exports.basicSetup = basicSetup;
--- src/basic-setup.ts
+++ src/basic-setup.ts
@@ -1,81 +1,59 @@
-import {keymap, highlightSpecialChars, drawSelection, highlightActiveLine} from "@codemirror/view"
-import {Extension, EditorState} from "@codemirror/state"
-import {history, historyKeymap} from "@codemirror/history"
-import {foldGutter, foldKeymap} from "@codemirror/fold"
-import {indentOnInput} from "@codemirror/language"
-import {lineNumbers, highlightActiveLineGutter} from "@codemirror/gutter"
-import {defaultKeymap} from "@codemirror/commands"
-import {bracketMatching} from "@codemirror/matchbrackets"
-import {closeBrackets, closeBracketsKeymap} from "@codemirror/closebrackets"
-import {searchKeymap, highlightSelectionMatches} from "@codemirror/search"
-import {autocompletion, completionKeymap} from "@codemirror/autocomplete"
-import {commentKeymap} from "@codemirror/comment"
-import {rectangularSelection} from "@codemirror/rectangular-selection"
-import {defaultHighlightStyle} from "@codemirror/highlight"
-import {lintKeymap} from "@codemirror/lint"
+import { EditorState, EditorSelection, Compartment } from "@codemirror/state"
+import { StreamLanguage } from "@codemirror/stream-parser"
+import { julia } from "@codemirror/legacy-modes/mode/julia"
+import { lineNumbers } from "@codemirror/gutter"
+import { keymap, EditorView, highlightSpecialChars, drawSelection, highlightActiveLine } from "@codemirror/view"
+import { historyKeymap, history } from "@codemirror/history"
+import { defaultKeymap, indentMore, indentLess } from "@codemirror/commands"
+import { defaultHighlightStyle } from "@codemirror/highlight"
+import { indentOnInput } from "@codemirror/language"
+import { rectangularSelection } from "@codemirror/rectangular-selection"
+import { foldGutter, foldKeymap } from "@codemirror/fold"
+import { bracketMatching } from "@codemirror/matchbrackets"
+import { closeBrackets, closeBracketsKeymap } from "@codemirror/closebrackets"
+import { autocompletion } from "@codemirror/autocomplete"
+import { highlightSelectionMatches, searchKeymap } from "@codemirror/search"
+import { completionKeymap } from "@codemirror/autocomplete"
+import { commentKeymap } from "@codemirror/comment"
 
-/// This is an extension value that just pulls together a whole lot of
-/// extensions that you might want in a basic editor. It is meant as a
-/// convenient helper to quickly set up CodeMirror without installing
-/// and importing a lot of packages.
-///
-/// Specifically, it includes...
-///
-///  - [the default command bindings](#commands.defaultKeymap)
-///  - [line numbers](#gutter.lineNumbers)
-///  - [special character highlighting](#view.highlightSpecialChars)
-///  - [the undo history](#history.history)
-///  - [a fold gutter](#fold.foldGutter)
-///  - [custom selection drawing](#view.drawSelection)
-///  - [multiple selections](#state.EditorState^allowMultipleSelections)
-///  - [reindentation on input](#language.indentOnInput)
-///  - [the default highlight style](#highlight.defaultHighlightStyle) (as fallback)
-///  - [bracket matching](#matchbrackets.bracketMatching)
-///  - [bracket closing](#closebrackets.closeBrackets)
-///  - [autocompletion](#autocomplete.autocompletion)
-///  - [rectangular selection](#rectangular-selection.rectangularSelection)
-///  - [active line highlighting](#view.highlightActiveLine)
-///  - [active line gutter highlighting](#gutter.highlightActiveLineGutter)
-///  - [selection match highlighting](#search.highlightSelectionMatches)
-///  - [search](#search.searchKeymap)
-///  - [commenting](#comment.commentKeymap)
-///  - [linting](#lint.lintKeymap)
-///
-/// (You'll probably want to add some language package to your setup
-/// too.)
-///
-/// This package does not allow customization. The idea is that, once
-/// you decide you want to configure your editor more precisely, you
-/// take this package's source (which is just a bunch of imports and
-/// an array literal), copy it into your own code, and adjust it as
-/// desired.
-export const basicSetup: Extension = [
-  lineNumbers(),
-  highlightActiveLineGutter(),
-  highlightSpecialChars(),
-  history(),
-  foldGutter(),
-  drawSelection(),
-  EditorState.allowMultipleSelections.of(true),
-  indentOnInput(),
-  defaultHighlightStyle.fallback,
-  bracketMatching(),
-  closeBrackets(),
-  autocompletion(),
-  rectangularSelection(),
-  highlightActiveLine(),
-  highlightSelectionMatches(),
-  keymap.of([
-    ...closeBracketsKeymap,
-    ...defaultKeymap,
-    ...searchKeymap,
-    ...historyKeymap,
-    ...foldKeymap,
-    ...commentKeymap,
-    ...completionKeymap,
-    ...lintKeymap
-  ])
+const basicSetup = [
+    lineNumbers(),
+    highlightSpecialChars(),
+    history(),
+    foldGutter(),
+    drawSelection(),
+    EditorState.allowMultipleSelections.of(true),
+    indentOnInput(),
+    defaultHighlightStyle.fallback,
+    bracketMatching(),
+    closeBrackets(),
+    autocompletion(),
+    rectangularSelection(),
+    highlightActiveLine(),
+    highlightSelectionMatches(),
+    keymap.of([
+        ...closeBracketsKeymap,
+        ...defaultKeymap,
+        ...searchKeymap,
+        ...historyKeymap,
+        ...foldKeymap,
+        ...commentKeymap,
+        ...completionKeymap,
+        //    ...lint.lintKeymap,
+    ]),
 ]
-
-export {EditorView} from "@codemirror/view"
-export {EditorState} from "@codemirror/state"
+export {
+    EditorState,
+    EditorSelection,
+    Compartment,
+    EditorView,
+    basicSetup,
+    julia,
+    keymap,
+    history,
+    historyKeymap,
+    defaultKeymap,
+    StreamLanguage,
+    indentMore,
+    indentLess,
+}
